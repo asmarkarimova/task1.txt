2109f51ae3387165731585980739462a3720e1ff
asmar <126621069+asmarkarimova@users.noreply.github.com>
2023-03-27 19:55:06 +0400

    Add files via upload

---
+++ while.js
@@ -0,0 +1,35 @@
+// task 1
+// let number = 16267345
+// while(number > 100) {
+//     const reminder = number % 10
+//     console.log(reminder)
+//     const n1 = number - reminder
+//     const n2 = n1 / 1000
+//     const n3 = n2 % 10
+//     number = parseInt(number/10)
+// }
+// console.log(number%10)
+
+// task 2
+// let number = 213457
+// let sum = 0
+// let last = number%10
+// while(number>10){
+//     number=parseInt(number/10)
+// }
+// sum=number+last
+// console.log(sum)
+
+// // task 3 
+// let number = 45678319
+// let num = 0
+// if(number>0){
+//     for(let i=0; i<number.toString() .length; i++){
+//         num+=1
+//     }
+// }
+// else {
+//     console.log("Wrong number")
+
+// }
+// console.log(num)
